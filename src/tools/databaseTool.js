4f7eb4fcb9fdb3b7776c3865d3c7a9c1944813fc
chenyuqi <958925285@qq.com>
2018-07-14 13:32:58 +0800

    数据库操作的进一步封装

--- src/tools/databaseTool.js
+++ src/tools/databaseTool.js
@@ -5,6 +5,23 @@ const URL = 'mongodb://localhost:27017';
 // 数据库名称
 const DBNAME = 'szhmqd18';
 
+// 获取数据库连接
+getDB = (backcall) => {
+    // 连接数据库
+    MongoClient.connect(URL, {
+        useNewUrlParser: true
+    }, function (err, client) {
+        if (err) {
+            console.log(err);
+        }
+        // 创建db对象  作为参数传给回调
+        const db = client.db(DBNAME)
+        backcall(err, db);
+        // 操作完数据后 关闭数据库连接
+        client.close();
+    });
+}
+
 
 /**
  * 暴露查询一条数据的方法
@@ -13,54 +30,34 @@ const DBNAME = 'szhmqd18';
  * @param {回调函数} backcall 
  */
 exports.findOne = (collectionName, data, backcall) => {
-    // 连接数据库
-    MongoClient.connect(URL, {
-        useNewUrlParser: true
-    }, function (err, client) {
-        const db = client.db(DBNAME);
-        // 需要查询的文档
-        const collection = db.collection(collectionName);
-        // 查询用户名
-        collection.findOne(data, function (err, docs) {
-            // 关闭数据库连接
-            client.close();
+    // 调用获取连接数据库方法
+    getDB((err, db) => {
+        // 查询一条数据
+        db.collection(collectionName).findOne(data, function (err, docs) {
             // 调用回调函数
             backcall(err, docs);
         })
-    });
+    })
 }
 
 // 暴露插入一条数据的方法
 exports.insertOne = (collectionName, data, backcall) => {
-    // 连接数据库
-    MongoClient.connect(URL, {
-        useNewUrlParser: true
-    }, function (err, client) {
-        const db = client.db(DBNAME);
-        // 需要查询的文档
-        const collection = db.collection(collectionName);
-        // 查询用户名
-        collection.insertOne(data, function (err, docs) {
-            // 关闭数据库连接
-            client.close();
+    // 调用获取连接数据库方法
+    getDB((err, db) => {
+        // 插入一条数据
+        db.collection(collectionName).insertOne(data, function (err, docs) {
             // 调用回调函数
             backcall(err, docs);
         })
-    });
+    })
 }
 
 // 暴露查询多条数据的方法
 exports.find = (collectionName, data, backcall) => {
-    // 连接数据库
-    MongoClient.connect(URL, {
-        useNewUrlParser: true
-    }, function (err, client) {
-        const db = client.db(DBNAME);
-        const collection = db.collection(collectionName);
-        // 查询学生列表信息  模糊查询
-        collection.find(data).toArray(function (err, docs) {
-            // 关闭数据库连接
-            client.close();
+    // 调用获取连接数据库方法
+    getDB((err, db) => {
+        // 查询数据
+        db.collection(collectionName).find(data).toArray(function (err, docs) {
             // 调用回调函数
             backcall(err, docs);
         })
